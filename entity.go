e9aa1fbbe75693a34b9a2c371fc5e3e90cd4c8a1
Tom Bevan <tom.bevan@r3labs.io>
2017-07-05 17:23:01 +0100

    added query support for name + status

--- entity.go
+++ entity.go
@@ -62,6 +62,8 @@ func (e *Entity) Find() []interface{} {
 			db.Select(fields).Where("group_id = ?", e.GroupID).Order("version desc").Find(&entities)
 		} else if e.DatacenterID != 0 {
 			db.Select(fields).Where("datacenter_id = ?", e.DatacenterID).Order("version desc").Find(&entities)
+		} else if e.Name != "" && e.Status != "" {
+			db.Select(fields).Where("name = ?", e.Name).Where("status = ?", e.Status).Order("version desc").Find(&entities)
 		}
 	}
 
